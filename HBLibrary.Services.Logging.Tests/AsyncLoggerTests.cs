a74b0a297a41bd277fd88cdc877a3f3fe5b82cd7
Merwa Stefan <stefanbsns@gmail.com>
2024-01-22 13:31:35 +0100

    Fix logger tests

--- HBLibrary.Services.Logging.Tests/AsyncLoggerTests.cs
+++ HBLibrary.Services.Logging.Tests/AsyncLoggerTests.cs
@@ -8,79 +8,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HBLibrary.Services.Logging.Tests {
-    [TestClass]
-    public class AsyncLoggerTests {
-        private const string LogFile = "../../assets/asyncLogFile";
+namespace HBLibrary.Services.Logging.Tests; 
+[TestClass]
+public class AsyncLoggerTests {
+    private const string LogFile = "../../../assets/asyncLogFile";
 
-        [TestMethod]
-        public async Task AsyncLogger_LogToFile_Valid() {
-            LoggerRegistry registry = new LoggerRegistry();
-            LoggerFactory factory = new LoggerFactory(registry);
-            IAsyncLogger<AsyncLoggerTests> logger = factory.GetOrCreateAsyncLogger<AsyncLoggerTests>();
+    [TestMethod]
+    public async Task AsyncLogger_LogToFile_Valid() {
+        LoggerRegistry registry = new LoggerRegistry();
+        LoggerFactory factory = new LoggerFactory(registry);
+        IAsyncLogger<AsyncLoggerTests> logger = factory.GetOrCreateAsyncLogger<AsyncLoggerTests>();
 
-            registry.ConfigureLogger(logger, e => e
-                .AddFileTarget(LogFile, true)
-                .WithDisplayFormat(LogDisplayFormat.Minimal)
-                .Build());
-
-            string testString = "Testinfo";
-            await logger.InfoAsync(testString);
-            logger.Dispose();
-
-            string content = File.ReadAllText(LogFile);
-            Assert.AreNotEqual("", content);
-            File.WriteAllText(LogFile, "");
-        }
-
-        [TestMethod]
-        public async Task AsyncLogger_LogToMethod_Valid() {
-            LoggerRegistry registry = new LoggerRegistry();
-            LoggerFactory factory = new LoggerFactory(registry);
-
-            IAsyncLogger logger = factory.GetOrCreateAsyncLogger("TestCategory");
-            registry.ConfigureLogger(logger, e => e
-            .AddMethodTarget((f, _) => Console.WriteLine(f.ToFullString()))
+        registry.ConfigureLogger(logger, e => e
+            .AddFileTarget(LogFile, true)
+            .WithDisplayFormat(LogDisplayFormat.Minimal)
             .Build());
 
-            using (StringWriter sw = new StringWriter()) {
-                Console.SetOut(sw);
-                await logger.InfoAsync("Testinfo");
-                Assert.IsTrue(sw.ToString().EndsWith("Log Level: Info\nMessage: Testinfo\r\n"));
-            }
+        string testString = "Testinfo";
+        await logger.InfoAsync(testString);
+        logger.Dispose();
 
-            logger.Dispose();
+        string content = File.ReadAllText(LogFile);
+        Assert.AreNotEqual("", content);
+        File.WriteAllText(LogFile, "");
+    }
+
+    [TestMethod]
+    public async Task AsyncLogger_LogToMethod_Valid() {
+        LoggerRegistry registry = new LoggerRegistry();
+        LoggerFactory factory = new LoggerFactory(registry);
+
+        IAsyncLogger logger = factory.GetOrCreateAsyncLogger("TestCategory");
+        registry.ConfigureLogger(logger, e => e
+        .AddMethodTarget((f, _) => Console.WriteLine(f.ToFullString()))
+        .Build());
+
+        using (StringWriter sw = new StringWriter()) {
+            Console.SetOut(sw);
+            await logger.InfoAsync("Testinfo");
+            Assert.IsTrue(sw.ToString().EndsWith("Log Level: Info\nMessage: Testinfo\r\n"));
         }
 
-        [TestMethod]
-        public async Task AsyncLogger_LogToFile_CheckThreadSafety_Valid() {
-            LoggerRegistry registry = new LoggerRegistry();
-            LoggerFactory factory = new LoggerFactory(registry);
+        logger.Dispose();
+    }
 
-            registry.ConfigureRegistry(e => e.AddFileTarget(LogFile, false).Build());
-            IAsyncLogger logger1 = factory.GetOrCreateAsyncLogger("Logger1");
-            IAsyncLogger logger2 = factory.GetOrCreateAsyncLogger("Logger2");
+    [TestMethod]
+    public async Task AsyncLogger_LogToFile_CheckThreadSafety_Valid() {
+        LoggerRegistry registry = new LoggerRegistry();
+        LoggerFactory factory = new LoggerFactory(registry);
 
-            try {
-                Task log1 = WriteLog(logger1, 10, "test");
-                Task log2 = WriteLog(logger2, 10, "test2");
-                await Task.WhenAll(log1, log2);
-            }
-            catch (Exception ex) {
-                Assert.Fail(ex.ToString());
-            }
-            finally {
-                registry.Dispose();
-                File.WriteAllText(LogFile, "");
-            }
+        registry.ConfigureRegistry(e => e.AddFileTarget(LogFile, false).Build());
+        IAsyncLogger logger1 = factory.GetOrCreateAsyncLogger("Logger1");
+        IAsyncLogger logger2 = factory.GetOrCreateAsyncLogger("Logger2");
+
+        try {
+            Task log1 = WriteLog(logger1, 10, "test");
+            Task log2 = WriteLog(logger2, 10, "test2");
+            await Task.WhenAll(log1, log2);
         }
+        catch (Exception ex) {
+            Assert.Fail(ex.ToString());
+        }
+        finally {
+            registry.Dispose();
+            File.WriteAllText(LogFile, "");
+        }
+    }
 
-        private async Task WriteLog(IAsyncLogger logger, int iterations, string message) {
-            Random rnd = new Random();
-            for (int i = 0; i < iterations; i++) {
-                await Task.Delay(rnd.Next(1, 5)); // Random delay between 1 and 5 milliseconds
-                await logger.InfoAsync(message + "[" + i + "]");
-            }
+    private async Task WriteLog(IAsyncLogger logger, int iterations, string message) {
+        Random rnd = new Random();
+        for (int i = 0; i < iterations; i++) {
+            await Task.Delay(rnd.Next(1, 5)); // Random delay between 1 and 5 milliseconds
+            await logger.InfoAsync(message + "[" + i + "]");
         }
     }
 }
