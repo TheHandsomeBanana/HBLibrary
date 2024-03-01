72e63e6601dca9678521949bc50442af062f456b
Merwa Stefan <stefanbsns@gmail.com>
2024-03-01 21:34:26 +0100

    add async parallel

---
+++ HBLibrary.Common/Parallelism/AsyncParallel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HBLibrary.Common.Parallelism;
+public static class AsyncParallel {
+    // example usage: await ParallelForEachAsync(source, async func => list.Add(await func())
+    public static Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> func, int degreeOfParallelism = -1) {
+        async Task AwaitPartition(IEnumerator<T> partition) {
+            using (partition) {
+                while(partition.MoveNext())
+                    await func(partition.Current);
+            }
+        }
+
+        return Task.WhenAll(Partitioner.Create(source)
+            .GetPartitions(degreeOfParallelism)
+            .AsParallel()
+            .Select(AwaitPartition));
+    }
+
+    public static AsyncParallelQuery<T> AsAsyncParallel<T>(this IEnumerable<T> source, int degreeOfParallelism = -1) {
+        return new AsyncParallelQuery<T>(source, degreeOfParallelism);
+    }
+}
---
+++ HBLibrary.Common/Parallelism/AsyncParallelQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HBLibrary.Common.Parallelism;
+public class AsyncParallelQuery<T> {
+    private readonly IEnumerable<T> source;
+    private readonly int degreeOfParallelism;
+
+    public AsyncParallelQuery(IEnumerable<T> source, int degreeOfParallelism) {
+        this.source = source;
+        this.degreeOfParallelism = degreeOfParallelism;
+    }
+
+    public Task ForEachAsync(Func<T, Task> func) {
+        return AsyncParallel.ForEachAsync(source, func, degreeOfParallelism);
+    }
+}
