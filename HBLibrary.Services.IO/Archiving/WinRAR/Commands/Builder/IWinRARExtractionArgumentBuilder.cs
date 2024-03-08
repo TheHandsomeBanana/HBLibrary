8848c639ac1ebd9dea21ce839f94a9d0c284a9e7
Merwa Stefan <stefanbsns@gmail.com>
2024-03-08 21:30:32 +0100

    winrar extraction argument builder

--- HBLibrary.Services.IO/Archiving/WinRAR/Commands/Builder/IWinRARExtractionArgumentBuilder.cs
+++ HBLibrary.Services.IO/Archiving/WinRAR/Commands/Builder/IWinRARExtractionArgumentBuilder.cs
@@ -3,6 +3,13 @@ public interface IWinRARExtractionArgumentBuilder : IWinRARCommonArgumentsBuilde
     public IWinRARExtractionArgumentBuilder SetOverwriteMode(WinRAROverwriteMode overwriteMode); // -o- | -o+
     public IWinRARExtractionArgumentBuilder SetPassword(string password); // -p
     public IWinRARExtractionArgumentBuilder RecurseSubdirectories(); // -r
+    public IWinRARExtractionArgumentBuilder AppendArchiveNameToDestination(); // -ad
+    public IWinRARExtractionArgumentBuilder IgnoreEmptyDirectories(); // -ed
+    public IWinRARExtractionArgumentBuilder KeepBrokenFiles(); // -kb
+    public IWinRARExtractionArgumentBuilder IncludeFiles(IEnumerable<string> files); // -n
+    public IWinRARExtractionArgumentBuilder IncludeFiles(string pattern); // -n
+    public IWinRARExtractionArgumentBuilder ExcludeFiles(IEnumerable<string> files); // -x
+    public IWinRARExtractionArgumentBuilder ExcludeFiles(string pattern); // -x
 
 
 }
