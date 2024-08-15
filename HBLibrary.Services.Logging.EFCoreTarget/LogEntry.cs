ac7cadf9a45ce39ee50da7b02c3bbd04bfda2109
Merwa Stefan <stefanbsns@gmail.com>
2024-08-15 18:18:46 +0200

    Add EFCoreDatabase Target for Logging

---
+++ HBLibrary.Services.Logging.EFCoreTarget/LogEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HBLibrary.Services.Logging.EFCoreTarget;
+public class LogEntry {
+    public int Id { get; set; }
+    public required DateTime Date { get; set; }
+    public required string Category { get; set; }
+    public required string Level { get; set; }
+    public required string Message { get; set; }
+}
---
+++ HBLibrary.Services.Logging.EFCoreTarget/LoggingContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HBLibrary.Services.Logging.EFCoreTarget;
+public class LoggingContext : DbContext {
+    private readonly string tableName;
+    public DbSet<LogEntry> LogEntries { get; }
+
+    public LoggingContext(DbContextOptions<LoggingContext> options, string tableName = "Logs") : base(options) { 
+        this.tableName = tableName;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<LogEntry>().ToTable(tableName);
+        modelBuilder.Entity<LogEntry>().HasKey(e => e.Id);
+    }
+}
