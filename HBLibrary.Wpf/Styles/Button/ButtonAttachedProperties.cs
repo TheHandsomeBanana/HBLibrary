18bfc70053cd18572d9f13179949c5154cb7bed5
Merwa Stefan <stefanbsns@gmail.com>
2024-09-24 11:37:19 +0200

    Outsource State handling. Set null severity. Fix corner radius attached property

--- HBLibrary.Wpf/Styles/Button/ButtonAttachedProperties.cs
+++ HBLibrary.Wpf/Styles/Button/ButtonAttachedProperties.cs
@@ -2,7 +2,7 @@
 
 namespace HBLibrary.Wpf.Styles.Button;
 public static class ButtonAttachedProperties {
-    public static readonly DependencyProperty CornerRadiousProperty = DependencyProperty.RegisterAttached(
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
         "CornerRadius",
         typeof(CornerRadius),
         typeof(ButtonAttachedProperties),
@@ -10,10 +10,10 @@ public static class ButtonAttachedProperties {
     );
 
     public static void SetCornerRadius(UIElement element, CornerRadius value) {
-        element.SetValue(CornerRadiousProperty, value);
+        element.SetValue(CornerRadiusProperty, value);
     }
 
     public static CornerRadius GetCornerRadius(UIElement element) {
-        return (CornerRadius)element.GetValue(CornerRadiousProperty);
+        return (CornerRadius)element.GetValue(CornerRadiusProperty);
     }
 }
